b9343a43b1e4617e24230009a97bd4f455c3b65f
Matt McDonald <mattmcd1221@gmail.com>
2017-06-26 14:48:11 -0400

    add comments explaining steps

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//REQUIREMENTS
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -6,20 +7,25 @@ const data = require('./userData.js');
 
 const app = express();
 
+//Connecting mustache-express to the app engine and linking it to the views folder
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+//Tells app to use the body-parser module for json files
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//'extended: false' parses strings and arrays.
+//'extended: true' parses nested objects
 
+//Setting up the session data
 app.use(session({
   secret: 'butter churner',
   resave: false,
   saveUninitialized: true
 }));
 
-//authenticate function
+//authenticate function to check if the content in the username matches the content in the login.mustache
 function authenticate (req, username, password) {
   var authenticatedUser = data.users.find (function (user) {
     if (username === user.username && password === user.password) {
@@ -28,11 +34,12 @@ function authenticate (req, username, password) {
     } else {
       return false;
     }
+    //checks to see if the values match
     console.log(req.session);
     return req.session;
   })
 }
-
+//prints the index.mustache file to the page if there is a current session and its authenticated, if it's not, it redirects you to the login page
 app.get('/', function (req, res) {
   if (req.session && req.session.authenticated) {
   res.render('index');
@@ -40,9 +47,7 @@ app.get('/', function (req, res) {
   res.redirect('/login');
 }
 })
-
-
-//
+//request for the server to pull the value entered in the username and password and send it through the authenticate function, then tells it where to redirect
 app.post('/', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
@@ -52,16 +57,13 @@ app.post('/', function (req, res) {
   } else {
     res.redirect('/login');
   }
-
-
 });
-
+//establishes the /login page
 app.get('/login', function (req, res) {
   res.render('login');
 })
 
-
-
+//listens for the app and the port 3000
 app.listen(3000, function(){
   console.log('Started express application!')
 });
